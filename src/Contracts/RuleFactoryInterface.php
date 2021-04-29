39284ea7d443122ab16cbf6f2216237ccf838df8
Alan Ondra <r.alan.ondra@gmail.com>
2021-04-29 10:45:42 -0400

    Strip down RuleFactoryInterface.

--- src/Contracts/RuleFactoryInterface.php
+++ src/Contracts/RuleFactoryInterface.php
@@ -2,30 +2,22 @@
 
 namespace FigTree\Validation\Contracts;
 
+use Closure;
+
 interface RuleFactoryInterface
 {
 	/**
-	 * Get the name of the RuleInterface implementation.
-	 *
-	 * @return string
-	 */
-	public function getRuleClass(): string;
-
-	/**
-	 * Set the name of the RuleInterface implementation.
-	 *
-	 * @param string $ruleClass
-	 *
-	 * @return \FigTree\Validation\Contracts\RuleFactoryInterface
-	 */
-	public function setRuleClass(string $ruleClass): RuleFactoryInterface;
-
-	/**
 	 * Create a new Rule.
 	 *
-	 * @param ...mixed $args
+	 * @param int $filterType The filter type.
+	 * @param int $flags Bitewise set of filter flags.
+	 * @param array $options Filter options.
+	 * @param \Closure|null Callback method.
 	 *
 	 * @return \FigTree\Validation\Contracts\RuleInterface
+	 *
+	 * @see https://www.php.net/manual/en/filter.filters.php
+	 * @see https://www.php.net/manual/en/filter.filters.flags.php
 	 */
-	public function create(...$args): RuleInterface;
+	public function create(int $filterType, int $flags = 0, array $options = [], ?Closure $callback = null): RuleInterface;
 }
