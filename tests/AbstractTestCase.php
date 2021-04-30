bb1e728045135f16405139f6b93ead420562c328
Alan Ondra <r.alan.ondra@gmail.com>
2021-04-30 18:00:05 -0400

    Remove testing artifacts.

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -3,16 +3,8 @@
 namespace FigTree\Validation\Tests;
 
 use PHPUnit\Framework\TestCase;
-use FigTree\Validation\Tests\Concerns\ReadsTestData;
 
 abstract class AbstractTestCase extends TestCase
 {
-	use ReadsTestData;
-
-	public function __construct()
-	{
-		parent::__construct();
-
-		$this->setDataDir(__DIR__ . '/Data');
-	}
+	//
 }
