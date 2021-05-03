f9b2a9091e203fa2c50f19e76fac4ca35f22b19c
Alan Ondra <r.alan.ondra@gmail.com>
2021-05-03 12:09:07 -0400

    Add more detail to RuleFactoryTest.

--- tests/RuleFactoryTest.php
+++ tests/RuleFactoryTest.php
@@ -2,17 +2,43 @@
 
 namespace FigTree\Validation\Tests;
 
+use const FILTER_SANITIZE_FULL_SPECIAL_CHARS;
+
 use Closure;
+use FigTree\Validation\Contracts\RuleFactoryInterface;
 use FigTree\Validation\Contracts\RuleInterface;
+use FigTree\Validation\AbstractRuleFactory;
 use FigTree\Validation\RuleFactory;
 
 class RuleFactoryTest extends AbstractTestCase
 {
+	/**
+	 * @small
+	 */
+	public function testRuleFactory()
+	{
+		$ruleFactory = new RuleFactory();
+
+		$this->assertInstanceOf(RuleFactoryInterface::class, $ruleFactory);
+		$this->assertInstanceOf(AbstractRuleFactory::class, $ruleFactory);
+	}
+
+	/**
+	 * @small
+	 */
 	public function testAddSlashesRule()
 	{
-		$rule = $this->ruleFactory()->addSlashes();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->addSlashes();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_SANITIZE_ADD_SLASHES, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -22,11 +48,22 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_SANITIZE_ADD_SLASHES, $array['filter']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testCleanEmailRule()
 	{
-		$rule = $this->ruleFactory()->cleanEmail();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->cleanEmail();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_SANITIZE_EMAIL, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -36,11 +73,22 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_SANITIZE_EMAIL, $array['filter']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testCleanEncodedStringRule()
 	{
-		$rule = $this->ruleFactory()->cleanEncodedString();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->cleanEncodedString();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_SANITIZE_ENCODED, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -50,11 +98,22 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_SANITIZE_ENCODED, $array['filter']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testCleanFloatRule()
 	{
-		$rule = $this->ruleFactory()->cleanFloat();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->cleanFloat();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_SANITIZE_NUMBER_FLOAT, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -64,11 +123,22 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_SANITIZE_NUMBER_FLOAT, $array['filter']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testCleanFullSpecialChars()
 	{
-		$rule = $this->ruleFactory()->cleanFullSpecialChars();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->cleanFullSpecialChars();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_SANITIZE_FULL_SPECIAL_CHARS, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -78,11 +148,22 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_SANITIZE_FULL_SPECIAL_CHARS, $array['filter']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testCleanInt()
 	{
-		$rule = $this->ruleFactory()->cleanInt();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->cleanInt();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_SANITIZE_NUMBER_INT, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -92,11 +173,22 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_SANITIZE_NUMBER_INT, $array['filter']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testCleanSpecialChars()
 	{
-		$rule = $this->ruleFactory()->cleanSpecialChars();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->cleanSpecialChars();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_SANITIZE_SPECIAL_CHARS, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -106,11 +198,22 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_SANITIZE_SPECIAL_CHARS, $array['filter']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testCleanString()
 	{
-		$rule = $this->ruleFactory()->cleanString();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->cleanString();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_SANITIZE_STRING, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -120,11 +223,22 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_SANITIZE_STRING, $array['filter']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testCleanUnsafe()
 	{
-		$rule = $this->ruleFactory()->cleanUnsafe();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->cleanUnsafe();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_UNSAFE_RAW, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -134,11 +248,22 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_UNSAFE_RAW, $array['filter']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testCleanUrl()
 	{
-		$rule = $this->ruleFactory()->cleanUrl();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->cleanUrl();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_SANITIZE_URL, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -148,11 +273,22 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_SANITIZE_URL, $array['filter']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testValidBool()
 	{
-		$rule = $this->ruleFactory()->validBool();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->validBool();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_VALIDATE_BOOLEAN, $rule->getFilterType());
+		$this->assertEquals(FILTER_NULL_ON_FAILURE, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -165,11 +301,22 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_NULL_ON_FAILURE, ($array['flags'] & FILTER_NULL_ON_FAILURE));
 	}
 
+	/**
+	 * @small
+	 */
 	public function testValidDomain()
 	{
-		$rule = $this->ruleFactory()->validDomain();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->validDomain();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_VALIDATE_DOMAIN, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -179,11 +326,22 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_VALIDATE_DOMAIN, $array['filter']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testValidEmail()
 	{
-		$rule = $this->ruleFactory()->validEmail();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->validEmail();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_VALIDATE_EMAIL, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -193,11 +351,22 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_VALIDATE_EMAIL, $array['filter']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testValidFloat()
 	{
-		$rule = $this->ruleFactory()->validFloat();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->validFloat();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_VALIDATE_FLOAT, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -207,11 +376,22 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_VALIDATE_FLOAT, $array['filter']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testValidInt()
 	{
-		$rule = $this->ruleFactory()->validInt();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->validInt();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_VALIDATE_INT, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -221,11 +401,22 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_VALIDATE_INT, $array['filter']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testValidIpAddress()
 	{
-		$rule = $this->ruleFactory()->validIpAddress();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->validIpAddress();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_VALIDATE_IP, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -235,11 +426,22 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_VALIDATE_IP, $array['filter']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testValidMacAddress()
 	{
-		$rule = $this->ruleFactory()->validMacAddress();
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->validMacAddress();
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_VALIDATE_MAC, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+		$this->assertNull($rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -249,13 +451,31 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals(FILTER_VALIDATE_MAC, $array['filter']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testValidRegExp()
 	{
 		$pattern = '/^[a-z]+$/i';
 
-		$rule = $this->ruleFactory()->validRegExp($pattern);
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->validRegExp($pattern);
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_VALIDATE_REGEXP, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertNull($rule->getCallback());
+
+		$options = $rule->getOptions();
+
+		$this->assertIsArray($options);
+		$this->assertNotEmpty($options);
+
+		$this->assertArrayHasKey('regexp', $options);
+		$this->assertEquals($pattern, $options['regexp']);
 
 		$array = $rule->toArray();
 
@@ -273,13 +493,27 @@ class RuleFactoryTest extends AbstractTestCase
 		$this->assertEquals($pattern, $options['regexp']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testWithCallable()
 	{
 		$callable = [$this, 'validatorMethod'];
 
-		$rule = $this->ruleFactory()->withCallable($callable);
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->withCallable($callable);
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_CALLBACK, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+
+		$this->assertNotNull($rule->getCallback());
+		$this->assertInstanceOf(Closure::class, $rule->getCallback());
+		$this->assertEquals(Closure::fromCallable($callable), $rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -287,15 +521,33 @@ class RuleFactoryTest extends AbstractTestCase
 
 		$this->assertArrayHasKey('filter', $array);
 		$this->assertEquals(FILTER_CALLBACK, $array['filter']);
+
+		$this->assertArrayHasKey('options', $array);
+		$this->assertInstanceOf(Closure::class, $array['options']);
+		$this->assertEquals(Closure::fromCallable($callable), $array['options']);
 	}
 
+	/**
+	 * @small
+	 */
 	public function testWithClosure()
 	{
 		$closure = Closure::fromCallable([$this, 'validatorMethod']);
 
-		$rule = $this->ruleFactory()->withClosure($closure);
+		$ruleFactory = new RuleFactory();
+
+		$rule = $ruleFactory->withClosure($closure);
 
 		$this->assertInstanceOf(RuleInterface::class, $rule);
+
+		$this->assertEquals(FILTER_CALLBACK, $rule->getFilterType());
+		$this->assertEquals(0, $rule->getFlags());
+		$this->assertIsArray($rule->getOptions());
+		$this->assertEmpty($rule->getOptions());
+
+		$this->assertNotNull($rule->getCallback());
+		$this->assertInstanceOf(Closure::class, $rule->getCallback());
+		$this->assertEquals($closure, $rule->getCallback());
 
 		$array = $rule->toArray();
 
@@ -303,15 +555,14 @@ class RuleFactoryTest extends AbstractTestCase
 
 		$this->assertArrayHasKey('filter', $array);
 		$this->assertEquals(FILTER_CALLBACK, $array['filter']);
+
+		$this->assertArrayHasKey('options', $array);
+		$this->assertInstanceOf(Closure::class, $array['options']);
+		$this->assertEquals($closure, $array['options']);
 	}
 
 	public function validatorMethod($value)
 	{
 		return (is_string($value) && $value == 'foo');
-	}
-
-	protected function ruleFactory()
-	{
-		return new RuleFactory();
 	}
 }
